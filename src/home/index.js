5166f4e2d1dc85878338734e354b5f156d270a9a
Sere <85357970+SereMartinez@users.noreply.github.com>
2022-10-27 09:31:47 -0300

    Update index.js

--- src/home/index.js
+++ src/home/index.js
@@ -1,14 +1,24 @@
-import Button from "../Components/boton.js"
-import {useNavigate} from "react-router-dom"
+import Button from "../Components/boton.js";
+import {useNavigate} from "react-router-dom";
+import background from "../../src/assets/background.jpg";
+
 
 function Home (){
     const navigate=useNavigate();
+
     const onClickButton=(url) => {
         navigate(url);
     }
     return (
-<Button onclick={()=>onClickButton("/game")} text="Haz click para iniciar el juego"/>
-)
+        <section className='hero is-medium is-fullheight' style={{backgroundImage:'url(${background})',
+        backgroundSize:"cover"}}>;
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                  <Button onClick={()=>onClickButton("/game")} text="texto"/>
+                </div>   
+            </div>
+        </section>
+    )
 }
 
 export default Home;
