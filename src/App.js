6b8b02ec7c20c1024fb1d8195495b55cc8f0d309
Sere <85357970+SereMartinez@users.noreply.github.com>
2022-10-27 09:54:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from "./home";
 import Game from "./game";
 import 'bulma/css/bulma.min.css';
-
+import {useState} from 'react'
 
 function App() {
   return (
     <BrowserRouter>
+    <div>
+       <MiComponente number="6" text="mitexto"/>
+    </div>
     <Routes>
       <Route path="/" element ={<Home/>}/>
       <Route path="/game" element ={<Game/>}/>
@@ -17,5 +20,18 @@ function App() {
   );
 }
 
-export default App;
+function MiComponente ({number, text}) {
+  const [textColor, setTextColor] = useState("red")
+  return (
+    <>   
+        <span>{number}</span>
+        <br/> 
+        <span style={{color: textColor}}>{text}</span>
+        <button onClick={() => setTextColor("Blue")}>Cambiar de color</button>
+    </>
+  );
+}
 
+
+
+export default App;
